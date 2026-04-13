5a8597477a3dd8b1d48139842d71cdb6c99ed48e
Azat <azatkep118@gmail.com>
2026-04-13 19:47:13 +0300

    add schemas for all models

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -7,3 +7,10 @@ class CreateUserRequest(BaseModel):
     email: EmailStr
     password_hash: str
     role: UserRole = UserRole.USER
+
+
+class UpdateUserRequest(BaseModel):
+    username: str | None = None
+    email: EmailStr | None = None
+    password_hash: str | None = None
+    role: UserRole | None = None
