cd901cbe4cc02e8ed86443e932973f8f13e3c8f4
Azat <azatkep118@gmail.com>
2026-04-24 17:19:06 +0300

    start fastapi app

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Digitalizer",
+    description="Service for document generation from templates",
+    version="0.1.0",
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
